bd4981b2d4bb511d9898badbeebaba06a52ccc69
Niit <niit@q4os-desktop>
2022-09-09 12:01:54 +0530

    my commit2

--- Hello.java
+++ Hello.java
@@ -1,10 +1,10 @@
 public class Hello{
 	public static void main(String []args){
 		System.out.println("Hello Manish");
-<<<<<<< HEAD
-		System.out.println("New user");
-=======
 		System.out.println("Hello Diksha");
->>>>>>> 8ca3b88dff9a95ec6bc7f9983b3b94cc8f699bd1
+		for(int i = 1; i <= 10; i++)
+		{
+			System.out.println(i);
+		}
 	}
 }
